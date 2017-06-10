f4d8b9c35059a0fd12ceeb9bee40dacfb5018418
fimac <fimccawley@gmail.com>
2017-06-10 15:55:58 +1000

    added api connection via http party, search form on index page, updated url to be dynamic

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,9 +1,22 @@
 class GigsController < ApplicationController
 
+  def search
+    @search = params[:search]
+  end
+
   def index
-    @setlists = HTTParty.get('http://api.setlist.fm/rest/0.1/search/setlists.json?&artistName=sleater-kinney?&p=2', :headers =>{'Content-Type' => 'application/json'})
+    search = params[:search]
+    page = 10
+    url = "http://api.setlist.fm/rest/0.1/search/setlists.json?&artistName=#{@search}?&p=#{page}"
+    gigInfo = HTTParty.get(url, :headers =>{'Content-Type' => 'application/json'})
+      if @gigInfo
+        @gigInfo = gigInfo["setlists"]["setlist"]
+      end
+
+# @setlists["setlists"]["setlist"]
     # params :search
     # params :p
+    # ?&p=2
   end
 
   def show
