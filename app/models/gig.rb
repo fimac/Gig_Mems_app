cf933f5fba5d57a6a25ca9d484981cb186003b1a
fimac <fimccawley@gmail.com>
2017-06-12 14:47:58 +1000

    google maps api added to show user location and venue location of gigs in gig history

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,3 +1,12 @@
 class Gig < ApplicationRecord
   belongs_to :user, optional: true
+
+
+  # Geocoder
+  geocoded_by :model_venue
+  after_validation :geocode
+  # Geocoder
+  def model_venue
+    "#{ self.venue} #{ self.city } #{ self.state }"
+  end
 end
